dda953d6888ec6ced6ac42f8a3ba419d2a84b8cd
ibrahim_medkour <medkourbrahim08@gmial.com>
2025-12-25 11:49:31 +0000

    coed sql

--- realisation/realisation.sql
+++ realisation/realisation.sql
@@ -1,70 +1,31 @@
 
--- CREATE DATABASE blog_en_ligne;
--- USE blog_en_ligne;
+CREATE DATABASE blogLigne;
+USE blogLigne;
 
-
-
--- CREATE TABLE utilisateur (
---     id_utilisateur INT AUTO_INCREMENT PRIMARY KEY,
---     nom VARCHAR(100) NOT NULL,
---     email VARCHAR(100) NOT NULL UNIQUE,
---     mot_de_passe VARCHAR(255) NOT NULL
--- );
-
-
--- CREATE TABLE categorie (
---     id_categorie INT AUTO_INCREMENT PRIMARY KEY,
---     nom_categorie VARCHAR(100) NOT NULL,
---     description TEXT
--- );
-
-
-
--- CREATE TABLE article (
---     id_article INT AUTO_INCREMENT PRIMARY KEY,
---     titre VARCHAR(150) NOT NULL,
---     contenu TEXT NOT NULL,
---     date_publication DATE,
---     id_utilisateur INT NOT NULL,
---     id_categorie INT NOT NULL,
---     FOREIGN KEY (id_utilisateur) REFERENCES utilisateur(id_utilisateur),
---     FOREIGN KEY (id_categorie) REFERENCES categorie(id_categorie)
--- );
-
-
--- CREATE TABLE commentaire (
---     id_commentaire INT AUTO_INCREMENT PRIMARY KEY,
---     contenu TEXT NOT NULL,
---     date_publication DATE,
---     id_utilisateur INT NOT NULL,
---     id_article INT NOT NULL,
---     FOREIGN KEY (id_utilisateur) REFERENCES utilisateur(id_utilisateur), 
---     FOREIGN KEY (id_article) REFERENCES article(id_article) 
--- );
-
-
-
-
-
-
-CREATE DATABASE blog_en_ligne;
-USE blog_en_ligne;
+--tableau utilisateur--
 
 CREATE TABLE utilisateur (
     id_utilisateur INT AUTO_INCREMENT PRIMARY KEY,
     nom VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     mot_de_passe VARCHAR(255) NOT NULL
-) COMMENT = 'Table des utilisateurs du blog';
+) 
+COMMENT = 'Table des utilisateurs du blog';
+
+
+
+--tableau catégorie--
 
 CREATE TABLE categorie (
     id_categorie INT AUTO_INCREMENT PRIMARY KEY,
     nom_categorie VARCHAR(100) NOT NULL,
     description TEXT
-) COMMENT = 'Table des catégories des articles';
+) 
+COMMENT = 'Table des catégories des articles';
 
 
 
+--tableau article--
 
 CREATE TABLE article (
     id_article INT AUTO_INCREMENT PRIMARY KEY,
@@ -78,6 +39,10 @@ CREATE TABLE article (
 ) 
 COMMENT = 'Table des articles ';
 
+
+
+
+--tableau commentaire--
 CREATE TABLE commentaire (
     id_commentaire INT AUTO_INCREMENT PRIMARY KEY,
     contenu TEXT NOT NULL,
